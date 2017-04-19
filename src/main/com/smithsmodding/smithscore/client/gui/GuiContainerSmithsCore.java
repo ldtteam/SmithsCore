198da59c786c8bdf37bb8b039151b7484e1f292e
OrionOnline <oriondevelopment@outlook.com>
2017-04-19 21:25:14 +0200

    Added serialisation to GUI's to see if it produces proper data.

--- src/main/com/smithsmodding/smithscore/client/gui/GuiContainerSmithsCore.java
+++ src/main/com/smithsmodding/smithscore/client/gui/GuiContainerSmithsCore.java
@@ -6,6 +6,7 @@
 
 package com.smithsmodding.smithscore.client.gui;
 
+import com.google.gson.Gson;
 import com.smithsmodding.smithscore.SmithsCore;
 import com.smithsmodding.smithscore.client.gui.components.core.IGUIComponent;
 import com.smithsmodding.smithscore.client.gui.hosts.IGUIBasedComponentHost;
@@ -31,6 +32,8 @@ import org.lwjgl.input.Keyboard;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -83,6 +86,38 @@ public abstract class GuiContainerSmithsCore extends GuiContainer implements IGU
         setIsInitialized(true);
 
         Keyboard.enableRepeatEvents(true);
+
+        if (SmithsCore.isInDevenvironment())
+        {
+            SmithsCore.getLogger().warn("Serializing gui.");
+            BufferedWriter writer = null;
+            try
+            {
+                writer = new BufferedWriter(new FileWriter("./" + getID() + ".json"));
+                writer.write((new Gson()).toJson(this));
+            }
+            catch (IOException e)
+            {
+                SmithsCore.getLogger().error("Failed to access file.", e);
+            }
+            catch (Exception e)
+            {
+                SmithsCore.getLogger().error("Generic serialisation exception.", e);
+            }
+            finally
+            {
+                try
+                {
+                    if (writer != null)
+                    {
+                        writer.close();
+                    }
+                }
+                catch (IOException e)
+                {
+                }
+            }
+        }
     }
 
     @Override
